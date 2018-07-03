5890e2785dcad03f24ce18d7d8aff1b2da3c3b1d
Pedro Souza <pedrosouza888@gmail.com>
2018-07-03 10:54:45 -0300

    Adicionando filtro de hosts de email

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
 var app = new Vue({
   el: '#app',
   data: {
-    users: [] 
+    users: [],
+    hosts: [],
+    filterSelected: ""
+  },
+  created: function() {
+    // Carrega users do endpoint e lista de filtros
+    axios.get('http://jsonplaceholder.typicode.com/users')
+      .then(function (response) {
+        app.users = response.data;
+        app.hosts = app.getHosts();
+      });
+  },
+  methods: {
+    // dada a string retorna uma string com todos os caracteres depois do '@'
+    getHostFromEmail: function(email) {
+      // expressão regular pra filtrar a string do email
+      let re = /@\S+/;
+      let found = email.match(re);
+
+      return found[0];
+    },
+
+    // retorna um array com todos os hosts de email
+    getHosts: function(){
+      let arr = [];
+
+      for (var i in this.users) {
+        let host = this.getHostFromEmail(this.users[i].email);
+        arr.push(host);
+      }
+
+      return arr;
+    },
+
+    // retorna um boleano verificando se email pertence ao filtro selecionado
+    filterHost: function(email){
+      return (this.filterSelected === "") || email.endsWith(this.filterSelected);
+    }
   }
 })
-
-var getUsers = function() {
-  axios.get('http://jsonplaceholder.typicode.com/users')
-    .then(function (response) {
-      app.users = response.data;
-    });
-};
-
-getUsers();
